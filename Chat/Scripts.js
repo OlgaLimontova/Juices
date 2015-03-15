339c286abd6aeff1a83a1ba48ab161fdaf44e94b
MadGenius <mad_genius_1996@mail.ru>
2015-03-15 20:53:46 +0300

    Add js file

--- Chat/Scripts.js
+++ Chat/Scripts.js
@@ -1,19 +1,54 @@
-var name = "";
+var name;
+var oldName;
+var messageList = [];
+var actionList = [];
 
 function run() {
     var connectButton = document.getElementById('connect');
-    connectButton.addEventListener('click', saveName);
+    connectButton.addEventListener('click', setName);
     var sendButton = document.getElementById('enter');
     sendButton.addEventListener('click', enterMessage);
     var deleteButton = document.getElementById('delete');
     deleteButton.addEventListener('click', deleteMessage);
     var editButton = document.getElementById('edit');
     editButton.addEventListener('click', editMessage);
+    var showButton = document.getElementById('show');
+    showButton.addEventListener('click', showHistory);
+    var getNick = document.getElementById('getnick');
+    getNick.value = getName();
+    var history = document.getElementById('history');
+    var item = localStorage.getItem('list');
+    history.value = item;
 };
 
-function saveName() {
+function setName() {
     var nickname = document.getElementById('getnick');
     name = nickname.value;
+    var action = "New name: " + name;
+    actionList.push(action);
+    localStorage.setItem('list', JSON.stringify(actionList, " ", 4));
+};
+
+function getName() {
+    return name;
+};
+
+function restoreMessage() {
+    if (typeof (Storage) == "undefined") {
+        alert("Error!");
+        return;
+    }
+    var message = localStorage.getItem('message');
+    return message && JSON.parse(message);
+};
+
+function restoreAction() {
+    if (typeof (Storage) == "undefined") {
+        alert("Error!");
+        return;
+    }
+    var action = localStorage.getItem('list');
+    return action && JSON.parse(action);
 };
 
 function enterMessage() {
@@ -26,8 +61,29 @@ function enterMessage() {
     if (mes.value != "") {
         enterMes = mes.value;
         oldHistory = his.value;
-        his.value += name + ": " + enterMes + "\n";
+        his.value += "\n" + name + ": " + enterMes;
         mes.value = "";
+        var lastMessage = his.value;
+        oldName = name;
+        var action = name + " sent message " + enterMes;
+        messageList.push(name + ": " + enterMes);
+        localStorage.setItem('message', JSON.stringify(messageList, " ", 4));
+        actionList.push(action);
+        localStorage.setItem('list', JSON.stringify(actionList, " ", 4));
+    }
+};
+
+function showHistory() {
+    var show = document.getElementById('show');
+    var history = document.getElementById('history');
+    if (show.innerHTML == "Show history") {
+        show.innerHTML = "Return";
+        var item = localStorage.getItem('list');
+        history.value = item;
+    }
+    else if (show.innerHTML == "Return") {
+        show.innerHTML = "Show history";
+        history.value = "";
     }
 };
 
@@ -36,12 +92,17 @@ function deleteMessage() {
     del = document.getElementById('delete');
     if (his.value != "")
         his.value = oldHistory;
+    action = "Message" + " " + mes + " was deleted";
+    actionList.push(action);
+    localStorage.setItem('list', JSON.stringify(actionList, " ", 4));
 };
 
 function editMessage() {
     his = document.getElementById('history');
-    mes = document.getElementById('mesarea');
-    ed = document.getElementById('edit');
     his.value = oldHistory;
+    mes = document.getElementById('mesarea');
     mes.value = enterMes;
+    action = "Message " + enterMes + " was changed";
+    actionList.push(action);
+    localStorage.setItem('list', JSON.stringify(taskList, " ", 4));
 };
