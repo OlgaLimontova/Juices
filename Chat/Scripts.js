80b1371168fb3562704ecbd8954914f4334c3669
MadGenius <mad_genius_1996@mail.ru>
2015-03-16 23:00:37 +0300

    Add new version of js file

--- Chat/Scripts.js
+++ Chat/Scripts.js
@@ -1,35 +1,45 @@
 var name;
-var oldName;
+var oldMessage;
 var messageList = [];
 var actionList = [];
 
 function run() {
-    var connectButton = document.getElementById('connect');
-    connectButton.addEventListener('click', setName);
-    var sendButton = document.getElementById('enter');
-    sendButton.addEventListener('click', enterMessage);
-    var deleteButton = document.getElementById('delete');
-    deleteButton.addEventListener('click', deleteMessage);
-    var editButton = document.getElementById('edit');
-    editButton.addEventListener('click', editMessage);
-    var showButton = document.getElementById('show');
-    showButton.addEventListener('click', showHistory);
     var getNick = document.getElementById('getnick');
     var nickname = localStorage.getItem('name');
     name = nickname.toString().substring(1, nickname.toString().length - 1);
-    getNick.value = name;
-    var history = document.getElementById('history');
-    var item = localStorage.getItem('list');
-    history.value = item;
+    document.getElementById('getnick').value = name;
+    var messages = localStorage.getItem('message');
+    var first = messages.toString().indexOf("\u0022");
+    var last = messages.toString().lastIndexOf("\u0022");
+    var i = 0;
+    while (first != last) {
+	    var bufIndex = messages.toString().indexOf("\u0022", first + 1);
+	    var message = messages.toString().substring(first + 1, bufIndex);
+	    if (i % 2 == 0)
+	        messageList.push(message);
+	    first = bufIndex;
+	    i++;
+    }
+    reloadHistory();
+};
+
+function clean() {
+    messageList.length = 0;
+    reloadHistory();
 };
 
 function setName() {
     var nickname = document.getElementById('getnick');
+    if (nickname.value == "") {
+        alert("Input name!");
+        return;
+    }
     name = nickname.value;
+    storeName(name);
     var action = "New name: " + name;
-    localStorage.setItem('name', JSON.stringify(name, " ", 4));
+    localStorage.setItem('name', JSON.stringify(name));
     actionList.push(action);
-    localStorage.setItem('list', JSON.stringify(actionList, " ", 4));
+    localStorage.setItem('list', JSON.stringify(actionList));
 };
 
 function getName() {
@@ -41,16 +51,16 @@ function storeName(name) {
         alert("Error! LocalStorage is not accessible!");
         return;
     }
-    localStorage.setItem('name', JSON.stringify(name, " ", 4));
-}
+    localStorage.setItem('name', JSON.stringify(name));
+};
 
 function restoreName() {
     if (typeof(Storage) == "undefined") {
         alert("Error! LocalStorage is not accessible!");
         return;
     }
-    var name = localStorage.getItem('name');
-    return name && JSON.parse(name);
+    var item = localStorage.getItem('name');
+    return item && JSON.parse(item);
 };
 
 function storeMessage(message) {
@@ -58,16 +68,16 @@ function storeMessage(message) {
         alert("Error! LocalStorage is not accessible!");
         return;
     }
-    localStorage.setItem('message', JSON.stringify(message, " ", 4));
-}
+    localStorage.setItem('message', JSON.stringify(message));
+};
 
 function restoreMessage() {
     if (typeof(Storage) == "undefined") {
         alert("Error! LocalStorage is not accessible!");
         return;
     }
-    var message = localStorage.getItem('message');
-    return message && JSON.parse(message);
+    var item = localStorage.getItem('message');
+    return item && JSON.parse(item);
 };
 
 function storeAction(action) {
@@ -75,16 +85,16 @@ function storeAction(action) {
         alert("Error! LocalStorage is not accessible!");
         return;
     }
-    localStorage.setItem('list', JSON.stringify(action, " ", 4));
-}
+    localStorage.setItem('list', JSON.stringify(action));
+};
 
 function restoreAction() {
     if (typeof(Storage) == "undefined") {
         alert("Error! LocalStorage is not accessible!");
         return;
     }
-    var action = localStorage.getItem('list');
-    return action && JSON.parse(action);
+    var item = localStorage.getItem('list');
+    return item && JSON.parse(item);
 };
 
 function enterMessage() {
@@ -95,54 +105,81 @@ function enterMessage() {
         return;
     }
     if (mes.value != "") {
-        enterMes = mes.value;
-        oldHistory = his.value;
-        his.value += "\n" + name + ": " + enterMes;
-        mes.value = "";
-        var lastMessage = his.value;
-        oldName = name;
-        var action = name + " sent message " + enterMes;
+	    enterMes = mes.value;
         messageList.push(name + ": " + enterMes);
-        localStorage.setItem('message', JSON.stringify(messageList, " ", 4));
-        actionList.push(action);
-        localStorage.setItem('list', JSON.stringify(actionList, " ", 4));
+        reloadHistory();
+        mes.value = "";
+        storeMessage(messageList);
+        var action = name + " sent message " + enterMes;
+        storeAction(action);
     }
 };
 
-function getOldName() {
-    return oldName;
-}
-
-function showHistory() {
-    var show = document.getElementById('show');
+function reloadHistory() {
     var history = document.getElementById('history');
-    if (show.innerHTML == "Show history") {
-        show.innerHTML = "Return";
-        var item = localStorage.getItem('list');
-        history.value = item;
-    }
-    else if (show.innerHTML == "Return") {
-        show.innerHTML = "Show history";
-        history.value = "";
-    }
+    history.value = "";
+    for (var i = 0; i < messageList.length; i++)
+        history.value += messageList[i] + "\n";
 };
 
 function deleteMessage() {
-    his = document.getElementById('history');
-    del = document.getElementById('delete');
-    if (his.value != "")
-        his.value = oldHistory;
-    action = "Message" + " " + enterMes + " was deleted";
-    actionList.push(action);
-    localStorage.setItem('list', JSON.stringify(actionList, " ", 4));
+    if (document.getElementById("getnumber").value != "") {
+        var number = document.getElementById("getnumber").value;
+        if (number > messageList.length) {
+            alert("Incorrect index. Try again.");
+            document.getElementById("getnumber").value = "";
+            return;
+        }
+        if (isNaN(parseInt(number))) {
+            alert("Incorrect input. Try again.");
+            document.getElementById("getnumber").value = "";
+            return;
+        }
+        var toDelete = messageList[number - 1];
+        for (var i = number - 1; i < messageList.length - 1; i++)
+            messageList[i] = messageList[i + 1];
+        messageList.length--;
+        alert("Message " + toDelete + " was deleted.");
+        action = "Message " + toDelete + " was deleted";
+        actionList.push(action);
+        localStorage.setItem('list', JSON.stringify(actionList));
+        document.getElementById('getnumber').value = "";
+        reloadHistory();
+    }
+    else {
+        alert("Input number!");
+        return;
+    }
 };
 
-function editMessage() {
-    his = document.getElementById('history');
-    his.value = oldHistory;
-    mes = document.getElementById('mesarea');
-    mes.value = enterMes;
-    action = "Message " + enterMes + " was changed";
-    actionList.push(action);
-    localStorage.setItem('list', JSON.stringify(taskList, " ", 4));
+function editMessageFirst() {
+    if (document.getElementById('getnumber').value != "") {
+        index = document.getElementById('getnumber').value;
+        if (index > messageList.length) {
+            alert("Incorrect index. Try again.");
+            document.getElementById('getnumber').value = "";
+            return;
+        }
+        if (isNaN(parseInt(index))) {
+            alert("Incorrect input. Try again.");
+            document.getElementById('getnumber').value = "";
+            return;
+        }
+        document.getElementById('getnumber').value = "";
+        var toEdit = messageList[index - 1];
+        oldMessage = document.getElementById('mesarea').value;
+        var edit = document.getElementById('mesarea');
+        edit.value = toEdit;
+        document.getElementById('enter').setAttribute("onclick", "editMessageSecond();");
+    }
+};
+
+function editMessageSecond() {
+    alert("Message " + messageList[index - 1] + " was changed.");
+    action = "Message " + messageList[index - 1] + " was changed";
+    storeAction(action);
+    messageList[index - 1] = document.getElementById('mesarea').value;
+    document.getElementById('mesarea').value = oldMessage;
+    document.getElementById('enter').setAttribute("onclick", "enterMessage();");
+    reloadHistory();
 };
